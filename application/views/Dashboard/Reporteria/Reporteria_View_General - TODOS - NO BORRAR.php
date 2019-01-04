73cb81b303ac118e337add10318b68b99d7e9964
Francisco Navas <navasfran98@gmail.com>
2019-01-04 08:56:00 -0600

    ocultar reportes

--- application/views/Dashboard/Reporteria/Reporteria_View_General - TODOS - NO BORRAR.php
+++ application/views/Dashboard/Reporteria/Reporteria_View_General - TODOS - NO BORRAR.php
@@ -63,11 +63,102 @@
 			<!--REPORTE 5-->
 
 
-			
+			<!--REPORTE 6-->
+			<form method="post" action="<?php echo base_url()?>Reporte6_controller/reporte6">
+				<div class="col-md-4">
+		         <!-- PANEL HEADLINE -->
+						<div class="panel panel-headline">
+								<div class="panel-heading">
+									<h3 class="panel-title"><i class="fa fa-print" aria-hidden="true"></i> Reporte de impresores</h3>
+								</div>
+								<div class="panel-body text-center">
+									<p>Reporte impresores que existen en el inventario</p>
+									<div class="row">
+										<div class="col-md-8">
+											<label>Unidad</label>
+											<select name="unidad3" id="unidad3" class="form-control">
+										<script type="text/javascript"> $('#unidad3').append('<option value="todo">Todo</option>');</script>
+
+											<?php for ($i=0; $i <count($unidades)-2 ; $i++): ?>
+												
+												<?php if($unidades[$i]['id_unidad'] != 4 && $unidades[$i]['id_unidad'] != 1 && $unidades[$i]['id_unidad'] != 38): ?>
+												            <option value="<?php echo $unidades[$i]['id_unidad'] ?>"><?php echo $unidades[$i]['unidad'] ?></option>
+											<?php endif; ?>
+
+											<?php endfor; ?>
+											</select>
+										</div>
+										<div class="col-md-4" style="padding-top: 10px">
+											<button type="submit"><img height="50" width="50" src="<?php echo base_url()?>assets/Reporteria/excel.png"></button>
+										</div>
+										
+									</div>
+									
+									
+								</div>
+						</div>
+				<!-- END PANEL HEADLINE -->
+		 		 </div>
+		 		</form>
+			<!--REPORTE 6-->
 
 
+			<!--REPORTE 8-->
+				<div class="col-md-4">
+		         <!-- PANEL HEADLINE -->
+						<div class="panel panel-headline">
+								<div class="panel-heading">
+									<h3 class="panel-title"><i class="fa fa-arrows" aria-hidden="true"></i> Reporte de movimientos</h3>
+								</div>
+								<div class="panel-body text-center">
+									<p>Reporte de todos los movimientos y asignaciones por año y por mes</p><br>
+									<div class="row">
+											<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalAno">Por año</button>
+											<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalMes">Por mes</button>
+									</div>
+									
+									
+								</div>
+						</div>
+				<!-- END PANEL HEADLINE -->
+		 		 </div>
+			<!--REPORTE 8-->
 
-        
+             
+			<!--REPORTE 11-->
+			<form method="post" action="<?php echo base_url()?>Reporte11_controller/reporte11">
+				<div class="col-md-4">
+		         <!-- PANEL HEADLINE -->
+						<div class="panel panel-headline">
+								<div class="panel-heading">
+									<h3 class="panel-title"><i class="fa fa-desktop" aria-hidden="true"></i> Reporte general por unidad</h3>
+								</div>
+								<div class="panel-body text-center">
+									<p>Reporte total del inventario es decir como se cierra el año por departamento (PC)</p>
+									<div class="row">
+										<div class="col-md-8">
+											<label>Unidad</label>
+											<select name="unidad" id="unidad" class="form-control">
+											<?php for ($i=0; $i <count($unidades) ; $i++): ?>
+												
+												<?php if($unidades[$i]['id_unidad'] != 4 && $unidades[$i]['id_unidad'] != 1 && $unidades[$i]['id_unidad'] != 38): ?>
+												            <option value="<?php echo $unidades[$i]['id_unidad'] ?>"><?php echo $unidades[$i]['unidad'] ?></option>
+											<?php endif; ?>
+
+											<?php endfor; ?>
+											</select>
+										</div>
+										<div class="col-md-4" style="padding-top: 10px">
+											<button type="submit"><img height="50" width="50" src="<?php echo base_url()?>assets/Reporteria/excel.png"></button>
+										</div>
+										
+									</div>
+								</div>
+						</div>
+				<!-- END PANEL HEADLINE -->
+		 		 </div>
+		 		 </form>
+			<!--REPORTE 11-->
 			
 
 			<!--REPORTE 7 por unidad-->
@@ -133,6 +224,32 @@
 			<!--REPORTE 7 por codigo-->
 
 
+			<!--REPORTE 4 por codigo-->
+				<div class="col-md-4">
+				<form method="post" action="<?php echo base_url()?>Reporte4_controller/reporte4">
+		         <!-- PANEL HEADLINE -->
+						<div class="panel panel-headline">
+								<div class="panel-heading">
+									<h3 class="panel-title"><i class="fa fa-shopping-cart" aria-hidden="true"></i></i> Reporte de compras</h3>
+								</div>
+								<div class="panel-body text-center">
+									<p>Reporte de compras por año para administración y laboratorios</p>
+									<div class="row">
+										<div class="col-md-8">
+											<label id="label"><span id="span">Año:</span></label>
+											<input required="" type="text" class="form-control" name="year">
+										</div>
+										<div class="col-md-4" style="padding-top: 10px">
+											<button type="submit"><img height="50" width="50" src="<?php echo base_url()?>assets/Reporteria/excel.png"></button>
+										</div>
+										
+									</div>
+								</div>
+						</div>
+				<!-- END PANEL HEADLINE -->
+				</form>
+		 		 </div>
+			<!--REPORTE 4 por codigo-->
 			
 
 
